9344f19133be6b9ba0944ab5b3e5b62bfccdb718
adriano A <abrantes.sistemas@gmail.com>
2026-04-05 22:01:19 -0300

    Ajuste pre-render pagina de details

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'imoveis/:id',
+    renderMode: RenderMode.Server // A página será gerada sob demanda no servidor
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender // O restante continua estático
   }
 ];
