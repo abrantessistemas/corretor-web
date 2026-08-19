14fac90bed1973b3db562b731984f13666653700
adriano A <abrantes.sistemas@gmail.com>
2026-08-19 20:02:36 -0300

    Atualizacao filtros exibicao do preco endereco

--- src/app/pages/properties/property-list/property-list.ts
+++ src/app/pages/properties/property-list/property-list.ts
@@ -13,6 +13,13 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { filter, map } from 'rxjs/operators';
+import { Dialog, DialogModule } from '@angular/cdk/dialog';
+import { PropertyFilter } from '../property-filter/property-filter';
+import { FormsModule } from '@angular/forms';
+import { MatOption } from '@angular/material/core';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelect } from '@angular/material/select';
 
 @Component({
   selector: 'app-property-list',
@@ -28,6 +35,14 @@ import { filter, map } from 'rxjs/operators';
     PropertySlide,
     MatTooltipModule,
     MatProgressBarModule,
+    DialogModule,
+    MatFormField,
+    MatLabel,
+    MatSelect,
+    MatOption,
+    MatInputModule,
+    FormsModule
+
   ],
   templateUrl: './property-list.html',
   styleUrl: './property-list.scss',
@@ -36,20 +51,31 @@ import { filter, map } from 'rxjs/operators';
 export class PropertyListComponent implements OnInit, OnDestroy {
   private router = inject(Router);
   public propertyService = inject(PropertyService);
+  setting = this.propertyService.settings();
 
   sortOrder = signal<'asc' | 'desc' | null>(null);
   loadDetails = false;
   selectedZone = signal<string | null>(null);
+  selectedBairro = signal<string | null>(null);
+  selectedDormitorio = signal<number | null>(null);
+  selectedMetragem = signal<number | null>(null);
   zonas = ['Zona Sul', 'Zona Norte', 'Zona Leste', 'Zona Oeste', 'Centro'];
+
 
   // Sinais para controlar os índices ativos de imagem por imóvel
   imageIndexes = signal<{ [key: number]: number }>({});
   private autoplayIntervalId: any;
+
   whatappNumber = this.propertyService.settings().whatsappConfig.whatsappNumber || '';
   whatsappMensagem = this.propertyService.settings().whatsappConfig.whatsappMessage || 'Olá! Gostaria de obter mais informações';
   whatsappContactName = this.propertyService.settings().whatsappConfig.whatsappContactName || 'Contato';
 
   whatsappUrl = `https://wa.me/${this.whatappNumber}?text=${encodeURIComponent(this.whatsappMensagem)}`;
+
+  bairros: any = [];
+  metragens: any = [];
+  dormitorios: any = [];
+
 
   ngOnInit(): void {
     const propertiesList = this.propertyService.properties() || [];
@@ -58,7 +84,14 @@ export class PropertyListComponent implements OnInit, OnDestroy {
         property.specs.area = [property.specs.area[0], property.specs.area[property.specs.area.length - 1]];
       }
     });
-
+    this.bairros = propertiesList.map(property => property.location.bairro).filter((value, index, self) => self.indexOf(value) === index);
+    this.bairros.sort((a: string, b: string) => a.localeCompare(b)); // Ordena alfabeticamente
+    this.metragens = [
+      ...new Set(
+        propertiesList.flatMap(property => property.specs.area)
+      )
+    ].sort((a, b) => a - b); // .sort() é opcional, caso queira ordenar os números
+    this.dormitorios = propertiesList.map(property => property.specs.bedrooms).filter((value, index, self) => self.indexOf(value) === index); // Remove duplicatas
     this.startImageAutoplay();
   }
 
@@ -97,15 +130,28 @@ export class PropertyListComponent implements OnInit, OnDestroy {
     let list = [...(this.propertyService.properties() || [])];
     const zone = this.selectedZone();
     const order = this.sortOrder();
+    const bairro = this.selectedBairro();
+    const area = this.selectedMetragem();
+    const dormitorios = this.selectedDormitorio();
 
     if (zone) {
-      list = list.filter(item => item.region === zone);
+      list = list.filter(item => item.location?.regiao === zone);
     }
 
     if (order) {
       list.sort((a, b) => order === 'asc' ? a.price - b.price : b.price - a.price);
     }
 
+    if (bairro) {
+      list = list.filter(item => item.location?.bairro === bairro);
+    }
+
+    if (area) {
+      list = list.filter(item => item.specs.area.find(a => a === area));
+    }
+    if (dormitorios) {
+      list = list.filter(item => item.specs.bedrooms === this.dormitorios);
+    }
     return list;
   });
 
@@ -147,4 +193,29 @@ export class PropertyListComponent implements OnInit, OnDestroy {
   filterByZone(zone: string) {
     this.selectedZone.update(current => current === zone ? null : zone);
   }
+
+  dialog = inject(Dialog);
+
+  openDialog() {
+    this.dialog.open(PropertyFilter, {
+      minWidth: '300px',
+      data: {
+        animal: 'panda',
+      },
+    });
+  }
+
+}
+
+@Component({
+  selector: 'cdk-dialog-data-example-dialog',
+  template: `
+    <div class="container" style="background-color: #f5f5f5; padding: 2rem; border-radius: 8px;">
+      <h1>Meu Título</h1>
+      <p>Este HTML está direto no arquivo .ts!</p>
+    </div>
+  `,
+})
+export class CdkDialogDataExampleDialog {
+  // data = inject(DIALOG_DATA);
 }
